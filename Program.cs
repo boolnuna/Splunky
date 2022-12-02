64d3a88a16fdf7921141dede4962e17dde3dc913
boolnuna <95946052+boolnuna@users.noreply.github.com>
2022-12-02 16:56:39 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿global using System;
+global using StereoKit;
+
+SKSettings settings = new SKSettings
+{
+    appName = "Splunky",
+    assetsFolder = "add",
+    displayPreference = DisplayMode.Flatscreen
+};
+if (!SK.Initialize(settings))
+    Environment.Exit(1);
+
+// Renderer.EnableSky = false;
+// Renderer.ClearColor = Color.Hex(0xa9c6e2ff);
+// Renderer.SetClip(0.08f, 256f);
+
+SK.Run(() =>
+{
+
+});
+    
+
+/*
+
+*/
---
+++ README.md
@@ -0,0 +1,19 @@
+Batied
+
+start in forest
+climb tree
+use to be hunting spot
+cast line and animal eats is
+reel it in
+
+Ideas-
+horror game
+racing
+hand held game
+cooking
+mini tower defense
+puncher
+boshy 
+hot potatoe
+portal
+splunky
